581c158ab31cf6d4590f6b327a9679f8f7680f4c
CodingJozey <47428076+CodingJozey@users.noreply.github.com>
2019-05-02 12:30:47 -0400

    Update Human.java

--- src/Human.java
+++ src/Human.java
@@ -2,13 +2,13 @@
 public class Human {
 
 	// instance Variables
-	public int age;
-	public String name;
-	public int food; 
-	public double wealth;
-	public double hunger;
-	public boolean hasVehicle;
-	Vehicle[] vehicles;
+	private int age;
+	private String name;
+	private int food; 
+	private double wealth;
+	private double hunger;
+	private boolean hasVehicle;
+	private Vehicle[] vehicles;
 
 
 	public Human()  { //Constructor new Human();
@@ -21,7 +21,31 @@ public class Human {
 		this.vehicles   = new Vehicle[100];
 
 	}
-	public int numberOfVehicles() {
+	public Human(String name, int age) {
+	        this.age        = 0;
+		this.name       ="";
+		this.wealth     =0.0;
+		this.hasVehicle = false;
+		this.hunger     = 5.0;
+		this.food       = 5;
+		this.vehicles   = new Vehicle[100];
+	}
+	public int getAge() {
+		return this.age;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public  void setAge(int age) {
+		this.name = age;
+	}
+	public void setName(String name) {
+		this.name= name;
+	}
+	public String toString() {
+		return "Human:[ name:"+getName()+" , age:"+getAge()+", wealth:"]
+	}
+			public int numberOfVehicles() {
 		int total = 0;
 		for (int i=0; i<vehicles.length; i++) {
 			if(vehicles[i]!=null)
@@ -37,17 +61,18 @@ public class Human {
 
 
 	public void eat() {
-		if (food>0) {
-			hunger--;
+		if (food>= 4.5) {
+			hunger-=1.0;
 			hunger -= 1.0;
-			food  -= 1.5;
+			food  -= 1;
 			System.out.println("Yummy, I ate all");
 		}
 
 
-		else System.out.println("Not enough please buy food");  
+		else System.out.println("Not enough food, please buy food");  
 	}
-
+public void eat(int food) {
+	hunger -= (int)(food/4.5);
 
 	public void grow() {
 		age++;
